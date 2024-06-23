56fc24709bc9d72738f54b4b566691e8965dbf7c
Gep <johnmicgepulla@gmail.com>
2024-06-23 21:02:38 +0800

    "Added Weather struct definition"

--- main.go
+++ main.go
@@ -11,6 +11,27 @@ import (
 	"github.com/fatih/color"
 )
 
-func main() {
-	fmt.Println("Hello World")
+type Weather struct {
+	Location struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+	} `json:"location"`
+	Current struct {
+		TempC    float64 `json:"temp_c"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+	Forecast struct {
+		Forecastday []struct {
+			Hour []struct {
+				TimeEpoch    int64   `json:"time_epoch"`
+				TempC        float64 `json:"temp_c"`
+				Condition    struct {
+					Text string `json:"text"`
+				} `json:"condition"`
+				ChanceOfRain float64 `json:"chance_of_rain"`
+			} `json:"hour"`
+		} `json:"forecastday"`
+	} `json:"forecast"`
 }
